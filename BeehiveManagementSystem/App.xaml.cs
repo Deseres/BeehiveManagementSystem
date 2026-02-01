ddcffd3015037f6c88936be17a27e960443cf8e6
vlad9 <vlad99zet@gmail.com>
2026-02-01 20:11:18 +0100

    Add project files.

---
+++ BeehiveManagementSystem/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BeehiveManagementSystem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ BeehiveManagementSystem/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace BeehiveManagementSystem;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        // This method loads the XAML. Do not remove it.
+        InitializeComponent();
+
+        // 1. Define your data
+        string[] jobs = { "Nectar Collector", "Honey Manufactorer", "Egg Care"};
+
+        // 2. Assign it to the picker
+        JobPicker.ItemsSource = jobs;
+    }
+
+    public void OnAssignJobButtonClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    public void OnWorkTheNextShiftButtonClicked(object sender, EventArgs e)
+    {
+    }
+
+    public void onOutOfHoneyButtonClicked(object sender, EventArgs e)
+    {
+    }
+}
